6d47b33d868e3935f319ae7e3ed8e4082fc6fc0d
PeteCastle <francis_mark_cayco@yahoo.com>
2025-07-15 13:13:21 +0800

    [DEV] Fix Formatting

--- src/get_hash.py
+++ src/get_hash.py
@@ -1,6 +1,6 @@
 import hashlib
+
 student_number = "2024-10356"
 salt = "MLOPS2025B"
-hashed_value = hashlib.sha256((student_number +
-salt).encode()).hexdigest()
+hashed_value = hashlib.sha256((student_number + salt).encode()).hexdigest()
 print(hashed_value)
